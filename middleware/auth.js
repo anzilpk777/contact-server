978bece4207204489148bb9b9d0fa5ae49de7bd7
Anzil <anzlpk777@gmail.com>
2026-03-31 19:48:26 +0530

    Updated project code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
-  const token = req.headers.authorization;
+  let token = req.headers.authorization;
 
   if (!token) return res.status(401).json({ msg: "No token" });
+
+  if (token.startsWith("Bearer ")) {
+    token = token.slice(7);
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
