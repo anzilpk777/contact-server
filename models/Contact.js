dc0efbab7715c4cf46cdf9911fca03388c10ea53
Anzil <anzlpk777@gmail.com>
2026-03-31 19:21:31 +0530

    Updated project code

--- models/Contact.js
+++ models/Contact.js
@@ -4,10 +4,13 @@ const contactSchema = new mongoose.Schema({
   name: String,
   email: String,
   phone: String,
+  address: String,
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+}, {
+  timestamps: true,
 });
 
 module.exports = mongoose.model("Contact", contactSchema);
